d885363af367a1d5b5469f8e70f7df927eec4b5d
tanagaku <aegidousi@outlook.jp>
2022-04-10 15:12:06 +0900

    add escape sample

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 def greeting(name):
     print("Hello",name)
+    #Escape
+    escapeStr = "\"I did nothing!\" he said. \"Not that either! Or the other thing.\""
+    print(escapeStr)
 
 greeting("World")
